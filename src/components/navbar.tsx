9288a746f59f5257cdf0c2f34f4e200c2b2f7e22
Musa001-web <ajibadetemilade@gmail.com>
2025-05-07 08:43:07 +0100

    fix: modify the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,14 +15,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export function Navbar() {
   return (
-    <header className="sticky top-0 z-30 flex h-28 w-full items-center justify-between border-b px-20 text-black">
+    <header className="sticky top-0 z-30 flex h-18 w-full items-center justify-between border-b border-gray-200 px-8 text-black">
       <div className="flex items-center gap-2 md:gap-4">
         <div className="relative flex-1 md:w-80 md:flex-none"></div>
       </div>
-      <div className="flex items-center gap-2 ">
-        <Button variant="ghost" size="icon" className="relative">
-          <Bell className="" size={15} />
-          <span className="absolute top-1.5 right-1.5 flex h-2 w-2 rounded-full bg-blue-600" />
+      <div className="flex items-center gap-1 ">
+        <Button variant="ghost" size="icon" className="relative rounded-full bg-[#F6F6F6]">
+          <Bell className="text-[#5D5D5D]" size={15} />
+          <span className="absolute top-1.5 right-1.5 flex h-2 w-2 rounded-full" />
           <span className="sr-only">Notifications</span>
         </Button>
         <DropdownMenu>
@@ -36,12 +36,12 @@ export function Navbar() {
                   src="/placeholder.svg?height=32&width=32"
                   alt="User"
                 />
-                <AvatarFallback>MA</AvatarFallback>
+                <AvatarFallback className="rounded-full bg-[#225BFF] text-white">MA</AvatarFallback>
               </Avatar>
               <ChevronDown className="text-muted-foreground" size={15} />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="text-black">
+          <DropdownMenuContent align="end" className="text-[#333333]">
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>Profile</DropdownMenuItem>
