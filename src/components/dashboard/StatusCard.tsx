45f3863479b05a87fcbe7bcdcfab0e655aea1a04
Gidado Richard <gidzdaquan@gmail.com>
2025-05-22 08:32:02 +0100

    feat: enhance StatusCard component with link navigation and update dashboard data structure

--- src/components/dashboard/StatusCard.tsx
+++ src/components/dashboard/StatusCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { CircleAlert, CircleCheckBig, CircleX, TrendingUp } from 'lucide-react';
 import { Card } from '@/components/ui/card';
 
@@ -14,6 +15,7 @@ interface StatusCardProps {
     icon: string;
     iconBgColor: string;
     iconColor: string;
+    url?: string;
 }
 
 export const StatusCard: React.FC<StatusCardProps> = ({
@@ -27,6 +29,7 @@ export const StatusCard: React.FC<StatusCardProps> = ({
     icon,
     iconBgColor,
     iconColor,
+    url,
 }) => {
     const renderIcon = () => {
         const className = `${iconBgColor} p-3 rounded-full ${iconColor}`;
@@ -42,9 +45,11 @@ export const StatusCard: React.FC<StatusCardProps> = ({
         }
     };
 
-    return (
-        <Card className={`flex-row items-center justify-between p-4 rounded-xl ${bgColor} ${textColor} border ${borderColor} shadow-xs hover:shadow-sm transition-shadow`}>
-            <div className='flex flex-col gap-3'>
+    const cardContent = (
+        <Card
+            className={`flex-row items-center justify-between p-4 rounded-xl h-40 ${bgColor} ${textColor} border ${borderColor} shadow-xs hover:shadow-sm transition-shadow ${url ? 'cursor-pointer' : 'cursor-default'}`}
+        >
+            <div className="flex flex-col gap-3">
                 <h3 className="text-base font-semibold">{title}</h3>
                 <p className="text-lg font-semibold flex items-center gap-2">
                     {value}{' '}
@@ -54,7 +59,15 @@ export const StatusCard: React.FC<StatusCardProps> = ({
                     </span>
                 </p>
             </div>
-            <div className={`${bgColor.replace('50', '100')} rounded-full p-2`}>{renderIcon()}</div>
+            <div className={`${iconBgColor} rounded-full p-2`}>{renderIcon()}</div>
         </Card>
+    );
+
+    return url ? (
+        <Link href={url} passHref aria-label={`Navigate to ${title} page`}>
+            {cardContent}
+        </Link>
+    ) : (
+        cardContent
     );
 };
