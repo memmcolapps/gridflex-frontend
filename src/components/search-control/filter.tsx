f2924b7efaa73f6857524d714a87b9e6d84fe978
Gidado Richard <gidzdaquan@gmail.com>
2025-07-08 11:40:59 +0100

    fix: Adjust checkbox styles for checked state and update FilterControl dropdown positioning

--- src/components/search-control/filter.tsx
+++ src/components/search-control/filter.tsx
@@ -153,7 +153,7 @@ export function FilterControl({
             {isOpen && (
                 <div
                     ref={dropdownRef}
-                    className="absolute z-50 mt-2 w-[250px] bg-white rounded-md shadow-lg border border-gray-200 p-4"
+                    className="absolute z-50 mt-2 w-[250px] bg-white rounded-md shadow-lg border border-gray-200 p-4 ml-[-45px]"
                 >
                     {filterType === "status" ? (
                         // Status Filter UI
--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -11,10 +11,10 @@ function Checkbox({
   return (
     <CheckboxPrimitive.Root
       data-slot="checkbox"
-      className={cn(
-        "round-full peer border-input dark:bg-input/30 data-[state=checked]:bg-green-700 data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-green-700 data-[state=checked]:border-green-700 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-none border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
-        className
-      )}
+       className={cn(
+    "round-full peer border-input dark:bg-input/30 data-[state=checked]:bg-blue-500 data-[state=checked]:text-white dark:data-[state=checked]:bg-blue-500 data-[state=checked]:border-blue-500 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-none border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+    className
+  )}
       {...props}
     >
       <CheckboxPrimitive.Indicator
