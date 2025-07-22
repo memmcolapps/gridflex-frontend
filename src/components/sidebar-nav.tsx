0fe83022d613c404c21ce08931379dd689a6cc41
oseni abdulhaleem <oseniabdulhaleem@gmail.com>
2025-07-22 16:04:44 +0100

    changed the name to payments

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -134,7 +134,7 @@ export function SidebarNav() {
           ]
         },
         { title: "Billing", href: "/billing/billing" },
-        { title: "Payment History", href: "/billing/payment-history" },
+        { title: "Payments", href: "/billing/payments" },
       ],
     },
     {
