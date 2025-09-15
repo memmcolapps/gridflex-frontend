d866ec5e8a59359f0076ca61103c95b49ba3335e
malimikillua <malimi@memmcol.com>
2025-09-15 11:48:36 +0100

    refactor: improve code formatting in use-profile and use-user hooks

--- src/hooks/use-profile.ts
+++ src/hooks/use-profile.ts
@@ -1,27 +1,30 @@
 // @/hooks/use-profile.ts
 import { useMutation, QueryClient } from "@tanstack/react-query";
-import { updateProfile, type UpdateProfilePayload } from "../service/profile-service";
+import {
+  updateProfile,
+  type UpdateProfilePayload,
+} from "../service/profile-service";
 import { toast } from "sonner";
 
 const queryClient = new QueryClient();
 
 export const useUpdateProfile = () => {
-    return useMutation({
-        mutationFn: async (payload: UpdateProfilePayload) => {
-            const response = await updateProfile(payload);
-            if ("success" in response && !response.success) {
-                throw new Error(response.error);
-            }
-            return response;
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: ["user"],
-            });
-            toast.success("Profile updated successfully!");
-        },
-        onError: (error) => {
-            toast.error(error.message || "Failed to update profile.");
-        },
-    });
+  return useMutation({
+    mutationFn: async (payload: UpdateProfilePayload) => {
+      const response = await updateProfile(payload);
+      if ("success" in response && !response.success) {
+        throw new Error(response.error);
+      }
+      return response;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+      toast.success("Profile updated successfully!");
+    },
+    onError: (error) => {
+      toast.error(error.message || "Failed to update profile.");
+    },
+  });
 };
