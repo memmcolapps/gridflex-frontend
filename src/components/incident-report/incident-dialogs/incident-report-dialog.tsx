f63c79b0fb56f2958cde00d9801e9e2d3ba8f03a
macbook <macbook@Kitans-Pro.local>
2025-09-23 13:57:49 +0100

    Add: create incident api

--- src/components/incident-report/incident-dialogs/incident-report-dialog.tsx
+++ src/components/incident-report/incident-dialogs/incident-report-dialog.tsx
@@ -2,11 +2,28 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useCreateIncident } from "@/hooks/use-incident";
+import { useState } from "react";
 
 export default function IncidentDialog({ isOpen, onOpenChange }: {
     isOpen: boolean;
     onOpenChange: (open: boolean) => void;
 }) {
+    const { mutate: sendMessage, isPending } = useCreateIncident();
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = () => {
+        sendMessage(
+            {
+                message,
+            },
+            {
+                onSuccess: () => {
+                    setMessage("");
+                },
+            }
+        );
+    };
 
     return (
         <Dialog open={isOpen} onOpenChange={onOpenChange}>
@@ -23,14 +40,18 @@ export default function IncidentDialog({ isOpen, onOpenChange }: {
                         <Textarea
                             id="message"
                             name="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
                             className="focus:outline-none focus:ring-0 focus:border-transparent  h-50 border-gray-200"
                             placeholder="Message..."
                         />
                         <Button
+                            onClick={handleSubmit}
                             variant={"default"}
+                            disabled={isPending}
                             className="text-md w-full cursor-pointer gap-2 text-white px-8 py-6 font-semibold bg-[#161CCA]"
                         >
-                            Send
+                            {isPending ? "Sending..." : "Send"}
                         </Button>
                     </div>
                 </div>
