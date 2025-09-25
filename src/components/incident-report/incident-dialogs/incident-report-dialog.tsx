a66a8322d3f4aa207659715c7c48ce2fd65d8aee
macbook <macbook@Kitans-Pro.local>
2025-09-25 08:42:37 +0100

    Fix: weird error

--- src/components/incident-report/incident-dialogs/incident-report-dialog.tsx
+++ src/components/incident-report/incident-dialogs/incident-report-dialog.tsx
@@ -55,10 +55,6 @@ export default function IncidentDialog({ isOpen, onOpenChange }: {
                             className="text-md w-full cursor-pointer gap-2 text-white px-8 py-6 font-semibold bg-[#161CCA]"
                         >
                             {isPending ? "Sending..." : "Send"}
-                            variant={"default"}
-                            className="text-md w-full cursor-pointer gap-2 text-white px-8 py-6 font-semibold bg-[#161CCA]"
-                        >
-                            Send
                         </Button>
                     </div>
                 </div>
