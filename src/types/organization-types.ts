e794dc4097445eca4a81f677d2b4ce3654dc115e
malimikillua <malimi@memmcol.com>
2025-10-14 09:21:33 +0100

    Add organization service functions and hooks for creating and updating RegionBhubServiceCenter and SubstationTransformerFeeder
    - Implemented `createRegionBhubServiceCenter` and `updateRegionBhubServiceCenter` functions in the organization service.
    - Added `useCreateRegionBhubServiceCenter` and `useUpdateRegionBhubServiceCenter` hooks for managing mutations.
    - Introduced `createSubstationTransfomerFeeder` and `updateSubstationTransfomerFeeder` functions along with their respective hooks.
    - Defined new types for payloads in `organization-types.ts` to support the new functionalities.

---
+++ src/types/organization-types.ts
@@ -0,0 +1,58 @@
+export interface CreateRegionBhubServiceCenterPayload {
+  parentId: string;
+  regionId: string;
+  name: string;
+  phoneNo: string;
+  email: string;
+  contactPerson: string;
+  address: string;
+  type: string;
+}
+
+export interface CreateOrgResponse {
+  responsecode: string;
+  responsedesc: string;
+  responsedata: string;
+}
+
+export interface UpdateRegionBhubServiceCenterPayload {
+  nodeId: string;
+  regionId: string;
+  name: string;
+  phoneNo: string;
+  email: string;
+  contactPerson: string;
+  address: string;
+  type: string;
+}
+
+export interface CreateSubstationTransfomerFeederPayload {
+  parentId: string;
+  name: string;
+  serialNo: string;
+  phoneNo: string;
+  email: string;
+  contactPerson: string;
+  address: string;
+  status: boolean;
+  voltage: string;
+  latitude: string;
+  longitude: string;
+  description: string;
+  type: string;
+}
+
+export interface UpdateSubstationTransfomerFeederPayload {
+  nodeId: string;
+  name: string;
+  serialNo: string;
+  phoneNo: string;
+  email: string;
+  contactPerson: string;
+  address: string;
+  status: boolean;
+  voltage: string;
+  latitude: string;
+  longitude: string;
+  description: string;
+}
