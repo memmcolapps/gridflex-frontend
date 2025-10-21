fe84f5c118aed794638555abb0dd2a27ff55bf05
macbook <macbook@Kitans-Pro.local>
2025-10-21 15:46:51 +0100

    Add: pagination

--- src/app/(protected)/customized-report/reports/customized-population.tsx
+++ src/app/(protected)/customized-report/reports/customized-population.tsx
@@ -5,6 +5,8 @@ import {
     TableHead,
     TableCell,
 } from "@/components/ui/table";
+import { Pagination, PaginationContent, PaginationItem, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useState } from "react";
 
 export default function CustomizedPopulation() {
@@ -146,6 +148,38 @@ export default function CustomizedPopulation() {
                         </TableRow>
                     ))}
             </ReportTable>
+            <Pagination className="mt-4 flex items-center justify-between">
+                <div className="flex items-center space-x-2">
+                    <span className="text-sm font-medium">Rows per page</span>
+
+                    <Select>
+                        <SelectTrigger className="h-8 w-[70px]">
+                            <SelectValue placeholder="10" />
+                        </SelectTrigger>
+                        <SelectContent
+                            position="popper"
+                            side="top"
+                            align="center"
+                            className="mb-1 ring-gray-50"
+                        >
+                            <SelectItem value="10">10</SelectItem>
+                            <SelectItem value="24">24</SelectItem>
+                            <SelectItem value="48">48</SelectItem>
+                        </SelectContent>
+                    </Select>
+
+                    <span className="text-sm font-medium">1-10 of 75</span>
+                </div>
+
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </ReportContainer>
     );
 }
