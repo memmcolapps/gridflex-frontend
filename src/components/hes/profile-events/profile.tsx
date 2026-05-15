57fca9aa5f921707b482edb0a6652a88980aebae
Gidado Richard <gidzdaquan@gmail.com>
2026-05-15 15:05:57 +0100

    updated timestamp field in Profile component to use entryTimestamp

--- src/components/hes/profile-events/profile.tsx
+++ src/components/hes/profile-events/profile.tsx
@@ -328,7 +328,7 @@ export function Profile({ selectedHierarchy, selectedUnits }: ProfileProps) {
               return {
                 sn: startSn + index,
                 meterNo: profile.meterNumber,
-                time: profile.receivedAt,
+                time: profile.entryTimestamp,
                 ...rest,
               };
             },
